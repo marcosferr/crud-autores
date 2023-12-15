4b47632f71c8b0fd2f59ed544442fbccdca260e3
marcosferr <ferreiramarcosadrian@gmail.com>
2023-12-15 10:05:02 -0300

    Refactor App component and add routing functionality

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthorList from "./componentes/AuthorList.jsx";
+import AuthorEdit from "./componentes/AuthorEdit.jsx";
+import AuthorNew from "./componentes/AuthorNew.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <AuthorList />,
+      },
+      {
+        path: "/edit/:id",
+        element: <AuthorEdit />,
+      },
+      {
+        path: "/new",
+        element: <AuthorNew />,
+      },
+    ],
+  },
+  {
+    path: "/new",
+    element: <h1>Favorite authors</h1>,
+  },
+  {
+    path: "/edit/:id",
+    element: <h1>Favorite authors</h1>,
+  },
+  {
+    path: "/author/:id",
+    element: <h1>Favorite authors</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
